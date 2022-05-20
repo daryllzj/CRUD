e6e32966cf76b1ae0845291961cc14608a5338fe
Daryl Lee <daryllzj@gmail.com>
2022-05-20 17:20:36 +0800

    Added FormValidation Function

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let form = document.getElementById('form');
+let form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    formValidation();
     console.log("Button");
 });
+
+let formValidation =() => {
+    if (input.value === "")
+    {
+        console.log("Failure");
+    }
+    else{
+        console.log("Success");
+    }
+}

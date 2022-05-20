0e0944a3773a56c9b9aaa98327a593c9d49e2d05
Daryl Lee <daryllzj@gmail.com>
2022-05-20 17:59:58 +0800

    Added acceptData, uploadData

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
 let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     formValidation();
-    console.log("Button");
 });
 
 let formValidation =() => {
     if (input.value === "")
     {
-        console.log("Failure");
+        msg.innerHTML = "Textarea is empty";
     }
     else{
-        console.log("Success");
+        msg.innerHTML = "";
+        acceptData()
     }
+}
+
+let data = {};
+
+let acceptData =() => {
+    data["text"] = input.value;
+    uploadData()
+}
+
+let uploadData =() => {
+    posts.innerHTML += `
+    <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+    </div>
+    `;
+    input.value = "";
 }
